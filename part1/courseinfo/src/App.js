c9a977b843e09d597163b5786c026b326f8ab2f0
Saga- <danielorangerl@gmail.com>
2022-03-19 12:12:21 +0000

    tidied 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -26,32 +26,18 @@ const App = () => {
   )
 }
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+const Header = (props) => <h1>{props.course}</h1>
 
-const Content = (props) => {
-  return(
-    <div>
-      <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </div>
-  )
-}
+const Content = (props) => (
+  <div>
+    <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+    <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+    <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+  </div>
+)
 
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-  )
-}
+const Total = (props) => <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
 
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercise}</p>
-  )
-}
+const Part = (props) => <p>{props.part} {props.exercise}</p>
 
 export default App
