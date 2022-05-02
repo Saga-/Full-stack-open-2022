d9bf0fdcda273df3f4e9fa87a776f3fa2de516c6
Saga- <danielorangerl@gmail.com>
2022-05-02 12:18:18 +0100

    4.2

---
+++ part4/blog/app.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const mongoose = require('mongoose');
+const cors = require('cors')
+const blogRouter = require('./controllers/blog')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const app = express()
+
+logger.info('connecting to:', config.MONGODB_URI);
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => logger.info('connected to MongoDB'))
+  .catch(error => logger.error('error connecting to MongoDB:', error.message));
+
+// Before route handling
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger);
+// Routers
+app.use(blogRouter);
+// After route handling
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app
