8fd929b08dcc982a9b910ceb373af0498a7dbfcb
Saga- <danielorangerl@gmail.com>
2022-05-22 12:32:14 +0100

    4.19

--- part4/blog/controllers/blog.js
+++ part4/blog/controllers/blog.js
@@ -1,6 +1,7 @@
 const blogRouter = require('express').Router();
 const Blog = require('../models/Blog');
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -9,7 +10,14 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response, next) => {
   const body = request.body;
-  const user = await User.findById(body.userId);
+
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const user = await User.findById(decodedToken.id);
   console.log(user);
 
   if (!body.hasOwnProperty('likes')) {
@@ -47,5 +55,13 @@ blogRouter.put('/:id', async (request, response, next) => {
   const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true });
   return response.status(200).json(updatedBlog);
 })
+
+getTokenFrom = request => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null;
+}
 
 module.exports = blogRouter;
