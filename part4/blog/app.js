e8b4bc27b81e8c36479dafa3880a410a27b8c04d
Saga- <danielorangerl@gmail.com>
2022-05-22 12:21:55 +0100

    4.18

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const blogRouter = require('./controllers/blog');
 const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
 
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 // Routers
 app.use('/api/blogs', blogRouter);
 app.use('/api/users', usersRouter);
+app.use('/api/login', loginRouter);
 // After route handling
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
